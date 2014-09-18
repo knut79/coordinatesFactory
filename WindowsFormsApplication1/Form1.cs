b95f4a33116fba52bcedc78fa333abeeb29304f3
knuthp <knut.dullum@gmail.com>
2014-09-18 16:02:06 +0200

    fixed untracked files

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -286,7 +286,7 @@ namespace WindowsFormsApplication1
                     if (m_gh.CH.FarLeft())
                     {
                         m_gh.Game.MapProvider.Map.RectX = m_gh.Game.MapProvider.Map.Rectangle.X;
-                        // m_gh.Game.MapProvider.Map.RectX = 0;
+                        //m_gh.Game.MapProvider.Map.RectX = 0;
                         right = false;
                     }
                     else
